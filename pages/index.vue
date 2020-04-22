b500954825d65a99aa4f261d1315092b4f420431
Thomas Trott <ttrott@intera.de>
2020-04-23 00:19:29 +0200

    [CLEANUP] initial project formatting

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,43 @@
 <template>
-  <div class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        yazot
-      </h1>
-      <h2 class="subtitle">
-        My primo Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+	<div class="container">
+		<div>
+			<logo/>
+			<h1 class="title">
+				yazot
+			</h1>
+			<h2 class="subtitle">
+				My primo Nuxt.js project
+			</h2>
+			<div class="links">
+				<a
+					class="button--green"
+					href="https://nuxtjs.org/"
+					target="_blank"
+				>
+					Documentation
+				</a>
+				<a
+					class="button--grey"
+					href="https://github.com/nuxt/nuxt.js"
+					target="_blank"
+				>
+					GitHub
+				</a>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import Logo from '~/components/Logo.vue'
-  import {Component} from "~/node_modules/nuxt-property-decorator";
+	import Vue from 'vue'
+	import Logo from '~/components/Logo.vue'
+	import {Component} from "~/node_modules/nuxt-property-decorator";
 
-  @Component({
-    components: {
-      Logo
-    }
-  })
-  export default class Index extends Vue {
-  }
+	@Component({
+		components: {
+			Logo
+		}
+	})
+	export default class Index extends Vue {
+	}
 </script>
