f634ca87ea9705a1ee2c608a3421218d672e5ebb
Thomas Trott <ttrott@intera.de>
2020-04-23 00:14:49 +0200

    initial nuxt setup

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        yazot
+      </h1>
+      <h2 class="subtitle">
+        My primo Nuxt.js project
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import Logo from '~/components/Logo.vue'
+  import {Component} from "~/node_modules/nuxt-property-decorator";
+
+  @Component({
+    components: {
+      Logo
+    }
+  })
+  export default class Index extends Vue {
+  }
+</script>
